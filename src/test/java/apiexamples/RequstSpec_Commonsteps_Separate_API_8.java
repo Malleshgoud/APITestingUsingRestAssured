7ed9f91186d43a3e71126bacfe93c99b95f7e231
MalleshGoud <ailymallesh205@gmail.com>
2026-01-26 10:21:23 +0530

    Reqspec code updated for multiple bookings

--- src/test/java/apiexamples/RequstSpec_Commonsteps_Separate_API_8.java
+++ src/test/java/apiexamples/RequstSpec_Commonsteps_Separate_API_8.java
@@ -2,23 +2,30 @@ package apiexamples;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class RequstSpec_Commonsteps_Separate_API_8 {
 
-    @BeforeTest
-    public  void beforetest()
+    RequestSpecification reqspech;
+    @BeforeClass
+    public  void setuprequest()
     {
+        reqspech= RestAssured.given().log().all()
+                .baseUri("https://restful-booker.herokuapp.com/")
+
+                .contentType(ContentType.JSON);
 
     }
    @Test
-           public void test_resposne_extract_into_string() {
+           public void create_booking() {
 
 //Build request
      String resposbody= RestAssured.given()
-              .baseUri("https://restful-booker.herokuapp.com/")
-              .basePath("booking")
+              .spec(reqspech)
+             .basePath("booking")
               .body("{\n" +
                       "    \"firstname\": \"mallesh00021675erew76\",\n" +
                       "    \"lastname\": \"goud5675\",\n" +
@@ -30,7 +37,7 @@ public class RequstSpec_Commonsteps_Separate_API_8 {
                       "    },\n" +
                       "    \"additionalneeds\": \"super bowls\"\n" +
                       "}")
-              .contentType(ContentType.JSON)
+
               //hit request and get response
               .post()
               //validate the response
@@ -45,5 +52,37 @@ public class RequstSpec_Commonsteps_Separate_API_8 {
 
 
    }
+
+    @Test
+    public void update_booking() {
+
+//Build request
+        String resposbody= RestAssured.given()
+                .spec(reqspech)
+                .basePath("booking/1")
+                .body("{\n" +
+                        "    \"firstname\": \"mallsh883333\",\n" +
+                        "    \"lastname\": \"goud5675\",\n" +
+                        "    \"totalprice\": \"1000\",\n" +
+                        "    \"depositpaid\": true,\n" +
+                        "    \"bookingdates\": {\n" +
+                        "        \"checkin\": \"2018-01-01\",\n" +
+                        "        \"checkout\": \"2019-01-01\"\n" +
+                        "    },\n" +
+                        "    \"additionalneeds\": \"super bowls\"\n" +
+                        "}")
+
+                //hit request and get response
+                .post()
+                //validate the response
+                .then()
+                .extract()
+                .body() //even if u commenst this u will get response it is default one we can get resposne with extract() method
+                //.asString();
+                .asPrettyString();
+
+        System.out.println(resposbody);
+
+    }
 
 }
